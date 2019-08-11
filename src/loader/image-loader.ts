e9ec501b1c0518dea9add9a11039783b60afba77
André Furchner <storkner@gmail.com>
2019-08-11 20:57:22 +0200

    using worker only in Web

--- src/loader/image-loader.ts
+++ src/loader/image-loader.ts
@@ -2,33 +2,38 @@ function createWorker(f: () => void) {
 	return new Worker(URL.createObjectURL(new Blob([`(${f})()`])));
 }
 
-const worker = createWorker(() => {
-	self.addEventListener('message', e => {
-		const src = e.data.src;
-		const useOffscreen = e.data.useOffscreen;
+let worker: Worker;
 
-		fetch(src, { mode: 'cors' })
-			.then(response => response.blob())
-			.then(blob => createImageBitmap(blob))
-			.then(bitmap => {
-				let img;
+if (Worker) {
+	worker = createWorker(() => {
+		self.addEventListener('message', e => {
+			const src = e.data.src;
+			const useOffscreen = e.data.useOffscreen;
 
-				if (useOffscreen) {
-					const canvas = new OffscreenCanvas(bitmap.width, bitmap.height);
-					const ctx = <any>canvas.getContext('2d') as CanvasRenderingContext2D;
+			fetch(src, { mode: 'cors' })
+				.then(response => response.blob())
+				.then(blob => createImageBitmap(blob))
+				.then(bitmap => {
+					let img;
 
-					ctx.drawImage(bitmap, 0, 0);
-					img = new Uint8Array(ctx.getImageData(0, 0, bitmap.width, bitmap.height).data);
-					// @ts-ignore
-					self.postMessage({ src, img });
-				} else {
-					img = bitmap;
-					// @ts-ignore
-					self.postMessage({ src, img }, [img]);
-				}
-			});
+					if (useOffscreen) {
+						const canvas = new OffscreenCanvas(bitmap.width, bitmap.height);
+						const ctx = <any>canvas.getContext('2d') as CanvasRenderingContext2D;
+
+						ctx.drawImage(bitmap, 0, 0);
+						img = new Uint8Array(ctx.getImageData(0, 0, bitmap.width, bitmap.height).data);
+						// @ts-ignore
+						self.postMessage({ src, img });
+					} else {
+						img = bitmap;
+						// @ts-ignore
+						self.postMessage({ src, img }, [img]);
+					}
+				});
+		});
 	});
-});
+}
+
 
 function loadImageWithWorker(src: string, useOffscreen: boolean): Promise<Uint8Array> {
 	return new Promise((resolve, reject) => {
