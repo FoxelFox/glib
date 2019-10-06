c37d1fccfc623e6698c683ac130f02093dbd440a
André Furchner <storkner@gmail.com>
2019-10-06 20:46:47 +0200

    added dataByteLength for better performance

--- src/data/array-buffer-native.ts
+++ src/data/array-buffer-native.ts
@@ -9,10 +9,12 @@ export class ArrayBufferNative {
 	normalize: boolean;
 	stride: number;
 	offset: number;
+	dataByteLength: number;
 
-	constructor(data: ArrayBuffer, size: number, type: number, normalize?: boolean, stride?: number, offset?: number) {
+	constructor(data: ArrayBuffer, dataByteLength: number, size: number, type: number, normalize?: boolean, stride?: number, offset?: number) {
 
 		this.rawData = data;
+		this.dataByteLength = dataByteLength;
 		this.size = size;
 		this.type = type;
 		this.normalize = !!normalize;
@@ -23,9 +25,9 @@ export class ArrayBufferNative {
 		this.updateBuffer(data);
 	}
 
-	createArray(data: ArrayBuffer): ArrayBufferView {
+	createArray(data: ArrayBuffer) {
 		switch (this.type) {
-			case gl.FLOAT: return new Float32Array(data);
+			case gl.FLOAT:return new Float32Array(data);
 			case gl.INT: return new Int32Array(data);
 			case gl.UNSIGNED_BYTE: return new Uint8Array(data);
 			default: return new Uint8Array(data);
@@ -35,7 +37,11 @@ export class ArrayBufferNative {
 	updateBuffer(data: ArrayBuffer) {
 		this.rawData = data;
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-		gl.bufferData(gl.ARRAY_BUFFER, this.createArray(data), gl.STATIC_DRAW);
+		gl.bufferData(gl.ARRAY_BUFFER, this.createArray(data), gl.STATIC_DRAW, 0, this.dataByteLength);
 		gl.bindBuffer(gl.ARRAY_BUFFER, null);
+	}
+
+	getBufferLength() {
+
 	}
 }
